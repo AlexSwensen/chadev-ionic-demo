cd583453602c42635468b48e211a5e65386987b4
Alexander Swensen <alex.swensen@gmail.com>
2016-04-10 18:13:47 -0400

    Basic repeater of list.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,11 @@ angular.module('starter.controllers', [])
 
 .controller('HomeCtrl', function($scope) {
 
-  
+  $scope.list = [
+    'thing one',
+    'thing two',
+    'thing three'
+  ];
 
 
 });
